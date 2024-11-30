f8491c82307ed6cb1be2f3859cabd07b8ce72d2b
kunalMathuriya <kunalmathuria27@gmail.com>
2024-11-30 17:25:29 +0530

    Added DB update script

---
+++ eirs_sms_mno_notification_db_update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+. ~/.bash_profile
+source ${commonConfigurationFile} 2>/dev/null
+
+dbPassword=$(java -jar  ${pass_dypt} spring.datasource.password)
+
+conn="mysql -h${dbIp} -P${dbPort} -u${dbUsername} -p${dbPassword} ${appdbName}"
+
+`${conn} <<EOFMYSQL
+
+UPDATE sys_param SET description = 'Smart Consumer secret key used for Auth Token' WHERE tag = 'smart_consumer_secret';
+UPDATE sys_param SET description = 'Callback URL for Smart server' WHERE tag = 'smart_callback_url';
+UPDATE sys_param SET description = 'SMS URL for Seatel server' WHERE tag = 'seatel_sms_url';
+UPDATE sys_param SET description = 'SMS TPS for Seatel server' WHERE tag = 'seatel_sms_tps';
+UPDATE sys_param SET description = 'Callback URL for Seatel server' WHERE tag = 'seatel_callback_url';
+UPDATE sys_param SET description = 'SMSC code for Seatel server' WHERE tag = 'seatel_smsc_code';
+UPDATE sys_param SET description = 'SMSC code for Cellcard server' WHERE tag = 'cellcard_smsc_code';
+UPDATE sys_param SET description = 'Callback URL for Metfone server' WHERE tag = 'metfone_callback_url';
+UPDATE sys_param SET description = 'Callback URL for Cellcard server' WHERE tag = 'cellcard_callback_url';
+
+UPDATE cfg_feature_alert SET description = '<e> not found in configured MNOs msisdn series and marked as Default operator. Corrective action to update new msisdn series to be taken', feature = 'SMS Notification' WHERE alert_id = 'alert1207';
+UPDATE cfg_feature_alert SET feature = 'SMS Notification' WHERE alert_id = 'alert1203';
+
+
+EOFMYSQL`
+
+
+
+echo "tables creation completed."
+
+
+
+
+echo "                                             *
+                                                  ***
+                                                 *****
+                                                  ***
+                                                   *                           "
+echo "********************Thank You DB Process is completed now *****************"
